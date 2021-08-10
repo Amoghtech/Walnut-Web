555575a0465654c088d28b43e5c046af8677697f
Amogh <amogh.1923cs1192@kiet.edu>
2021-08-11 01:06:45 +0530

    Chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import CsvReader from './components/CSVReader'
+import CsvReader from './components/CSVReader';
 import './App.css';
-
+import Chart from './components/Chart';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <CsvReader />
+        <Chart />
       </header>
     </div>
   );
